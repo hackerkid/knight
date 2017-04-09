777fe343f6a24c440bc595ad9d33185312b9c96f
hackerkid <hackerkid@vishnuks.com>
2017-04-09 20:16:46 +0530

    First working proto.

---
+++ knight/__init__.py
@@ -0,0 +1,1 @@
+from .knight import app
---
+++ knight/schema.sql
@@ -0,0 +1,13 @@
+drop table if exists users;
+create table users (
+  id integer primary key autoincrement,
+  username text not null,
+  password text not null
+);
+
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  'text' text not null
+);
